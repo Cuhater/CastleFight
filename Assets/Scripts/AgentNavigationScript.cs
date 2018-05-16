5c28dc10831116ef05b76ead224e778defabb763
Cuhater <christian_krenn@hotmail.de>
2018-05-16 16:56:13 +0200

    updated Leveldesign

---
+++ Assets/Scripts/AgentNavigationScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentNavigationScript : MonoBehaviour {
+
+    private NavMeshAgent agent;
+   // public float navDistance = .2f;
+   // private int navPointCounter = -1;
+   // private bool needsNavPoints = true;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if(needsNavPoints || Vector3.Distance(gameObject.transform.position, NavPoints[]))
+
+
+        RaycastHit hit;
+        if( Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit))
+        {
+            agent.destination = hit.point;
+        }
+	}
+}
